fc0adb3fb13078beff42659861d69aaa566e77aa
Christian Bulacan <cbulacan9@gmail.com>
2014-12-09 14:00:57 -0500

    First quiz-app commit'

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quizApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quizApp
+ */
+angular.module('quizApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.quiz = [
+		{ 
+		  "q": "Who is the best ping pong player at FSA?", 
+		  'options': [{ 'value': "Mike"} , { 'value': "Eddie"} , {'value' : "Nimit"} , { 'value': "Patrick"}],
+		  'answer': "Nimit", 
+		  'difficulty':  2
+
+		},
+		{ "q": "Which robot name was chanted during Lego Mindstorms?", 
+		  'options':[{ 'value': 'infiniteLoop'} , { 'value': 'noHope.js'} , {'value' : 'johnny5'} , { 'value': 'none of the above'}], 
+		  'answer':'noHope.js',
+		  'difficulty': 5
+		},
+		{ 
+		  'q': "Out of the following frontend frameworks, which framework is most rails-like", 
+		  'options':[{ 'value': 'Ember.js'} ,{ 'value': 'Angular.js'} , {'value' : 'Backbone.js'} , { 'value': 'Meteor.js'}], 
+		  'answer':'Ember.js',
+		  'difficulty':  2
+		},
+		{ 
+		  'q': "Which project used a local data store?", 
+		  'options':[{ 'value': 'TripPlanner'} ,{ 'value': 'Twitter.js'} , {'value' : 'WikiWalker'} , { 'value': 'WikiStack'}], 
+		  'answer':'Twitter.js',
+		  'difficulty':  1
+		}
+	];
+
+	$scope.checkAnswer = function(guess, question) {
+		// console.log(guess.value == question.answer, guess, question);
+		if(guess.value == question.answer) {
+			alert('Correct!');
+			$scope.score++;
+		}
+		else {
+			alert('Wrong Answer');
+		}
+	}
+
+	$scope.score = 0;
+
+  });
+
+
